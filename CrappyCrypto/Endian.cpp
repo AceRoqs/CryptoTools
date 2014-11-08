783497c38ccbf58dcb87fd0f2d1ca3674e2fe821
Toby Jones <ace_roqs@yahoo.com>
2014-11-08 09:56:18 -0800

    Move endian implementation to cpp file.

---
+++ CrappyCrypto/Endian.cpp
@@ -0,0 +1,56 @@
+#include "PreCompile.h"
+#include "Endian.h"
+
+namespace PortableRuntime
+{
+
+// Change byte order from big to little or little to big.
+static uint16_t uint16_swap(uint16_t value)
+{
+    return ((value >> 8) & 0x00ff) | ((value << 8) & 0xff00);
+}
+
+static uint32_t uint32_swap(uint32_t value)
+{
+    return ((value >> 24) & 0x000000ff) | ((value >> 8) & 0x0000ff00) | ((value << 8) & 0x00ff0000) | ((value << 24) & 0xff000000);
+}
+
+// Change byte order from big/little to native format.
+uint16_t lswap16(uint16_t value)
+{
+#ifdef BIG_ENDIAN
+    return uint16_swap(value);
+#else
+    return value;
+#endif
+}
+
+uint16_t bswap16(uint16_t value)
+{
+#ifdef BIG_ENDIAN
+    return value;
+#else
+    return uint16_swap(value);
+#endif
+}
+
+uint32_t lswap32(uint32_t value)
+{
+#ifdef BIG_ENDIAN
+    return uint32_swap(value);
+#else
+    return value;
+#endif
+}
+
+uint32_t bswap32(uint32_t value)
+{
+#ifdef BIG_ENDIAN
+    return value;
+#else
+    return uint32_swap(value);
+#endif
+}
+
+}
+
--- CrappyCrypto/Endian.h
+++ CrappyCrypto/Endian.h
@@ -1,57 +1,13 @@
 #ifndef ENDIAN_H
 #define ENDIAN_H
 
-// TODO: Move implementations to a cpp file.
 namespace PortableRuntime
 {
 
-// Change byte order from big to little or little to big.
-inline uint16_t uint16_swap(uint16_t value)
-{
-    return ((value >> 8) & 0x00ff) | ((value << 8) & 0xff00);
-}
-
-inline uint32_t uint32_swap(uint32_t value)
-{
-    return ((value >> 24) & 0x000000ff) | ((value >> 8) & 0x0000ff00) | ((value << 8) & 0x00ff0000) | ((value << 24) & 0xff000000);
-}
-
-// Change byte order from big/little to native format.
-inline uint16_t lswap16(uint16_t value)
-{
-#ifdef BIG_ENDIAN
-    return uint16_swap(value);
-#else
-    return value;
-#endif
-}
-
-inline uint16_t bswap16(uint16_t value)
-{
-#ifdef BIG_ENDIAN
-    return value;
-#else
-    return uint16_swap(value);
-#endif
-}
-
-inline uint32_t lswap32(uint32_t value)
-{
-#ifdef BIG_ENDIAN
-    return uint32_swap(value);
-#else
-    return value;
-#endif
-}
-
-inline uint32_t bswap32(uint32_t value)
-{
-#ifdef BIG_ENDIAN
-    return value;
-#else
-    return uint32_swap(value);
-#endif
-}
+uint16_t lswap16(uint16_t value);
+uint16_t bswap16(uint16_t value);
+uint32_t lswap32(uint32_t value);
+uint32_t bswap32(uint32_t value);
 
 }
 
