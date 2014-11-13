4249dd1e5f047cf90ef369b5768aeb460c6f3216
Toby Jones <ace_roqs@yahoo.com>
2014-11-13 14:09:08 -0800

    Modernize the decryption code a bit.  Fix bug introduced a few weeks ago when the ciphertext is an exact multiple of the block length.

--- CrappyCrypto/Decrypt.cpp
+++ CrappyCrypto/Decrypt.cpp
@@ -9,71 +9,72 @@ namespace Skipjack
 
 int decrypt_main(int argc, char** argv)
 {
-    uint8_t keyvector[key_length] = {};
-    uint8_t testvector[block_length] = {};
-    uint8_t testvector2[block_length] = {};
-
-    int i, j;
-    FILE* pInfile;
-    FILE* pOutfile;
-
     if(argc != 4)
     {
         fprintf(stderr, "Usage: %s infile outfile key", argv[0]);
         exit(0);
     }
 
-    // Build key.
-    i = 0;
-    while(argv[3][i] != 0)
-    {
-        keyvector[i % key_length] ^= argv[3][i];
-        i++;
-    }
-
     // Open input file.
-    if((pInfile = fopen(argv[1], "rb")) == 0)
+    FILE* input_file = fopen(argv[1], "rb");
+    if(input_file == nullptr)
     {
         fprintf(stderr, "%s: error opening %s\n", argv[0], argv[1]);
         exit(1);
     }
 
     // Open output file.
-    if((pOutfile = fopen(argv[2], "wb")) == 0)
+    FILE* output_file = fopen(argv[2], "wb");
+    if(output_file == nullptr)
     {
-        fclose(pInfile);
+        fclose(input_file);
         fprintf(stderr, "%s: error opening %s\n", argv[0], argv[2]);
         exit(1);
     }
 
-    // Decrypt file.
-    i = (int)fread(testvector2, 1, block_length, pInfile);
-    if(i == block_length)
+    // Build key.
+    // This needs to match the code in Encrypt.cpp.
+    // TODO: Share this code.
+    // NOTE: Since the input is ASCII, most bits have no chance at being set, which limits the range of keys.
+    uint8_t keyvector[key_length] = {};
+    for(size_t ix = 0; argv[3][ix] != '\0'; ++ix)
     {
-        memcpy(testvector, testvector2, block_length);
-        j = (int)fread(testvector2, 1, block_length, pInfile);
+        keyvector[ix % key_length] ^= argv[3][ix];
+    }
 
-        while((i > 0) && !ferror(pInfile) && !ferror(pOutfile))
+    // Decrypt file.
+    uint8_t current_block[block_length];
+    uint8_t next_block[block_length];
+    size_t current_block_length = fread(next_block, 1, block_length, input_file);
+    if(current_block_length == block_length)
+    {
+        memcpy(current_block, next_block, block_length);
+        size_t next_block_length = fread(next_block, 1, block_length, input_file);
+
+        while((current_block_length > 0) && !ferror(input_file) && !ferror(output_file))
         {
-            decrypt(testvector, keyvector);
-            if(j == block_length)
+            decrypt(current_block, keyvector);
+            if(next_block_length == block_length)
             {
-                fwrite(testvector, 1, block_length, pOutfile);
+                fwrite(current_block, 1, block_length, output_file);
 
-                i = j;
-                memcpy(testvector, testvector2, block_length);
-                j = (int)fread(testvector2, 1, block_length, pInfile);
+                current_block_length = next_block_length;
+                memcpy(current_block, next_block, block_length);
+                next_block_length = fread(next_block, 1, block_length, input_file);
             }
             else
             {
-                fwrite(testvector, 1, testvector2[0] <= block_length ? testvector2[0] : 0, pOutfile);
+                if(next_block_length > 0)
+                {
+                    fwrite(current_block, 1, next_block[0] <= block_length ? next_block[0] : 0, output_file);
+                }
                 break;
             }
         }
     }
 
-    fclose(pInfile);
-    fclose(pOutfile);
+    fclose(input_file);
+    fclose(output_file);
 
     return 0;
 }
