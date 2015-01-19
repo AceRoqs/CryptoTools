a673c541e9bc8420eb6b82248b6d1e898d7b2dfd
Toby Jones <ace_roqs@yahoo.com>
2015-01-18 17:05:30 -0800

    Port in intrinsics and comments from a private project.

--- CrappyCrypto/Endian.cpp
+++ CrappyCrypto/Endian.cpp
@@ -1,18 +1,48 @@
 #include "PreCompile.h"
 #include "Endian.h"         // Pick up forward declarations to ensure correctness.
 
+/*
+For actual swaps, Visual C++ defines the following in intrin.h:
+    _byteswap_ushort
+    _byteswap_ulong
+    _byteswap_uint64
+GCC defines similar intrinsics:
+    __builtin_bswap32
+    __builtin_bswap64
+    (16-bit swap is simply a bit rotate)
+Clang doesn't appear to have intrinsics for this.
+
+To detect endianness at runtime, use the following.  The compiler should
+be able to optimize the function completely away, but check the assembly
+to be sure.
+
+bool is_big_endian()
+{
+    unsigned int x = 1;
+    return *reinterpret_cast<uint8_t*>(&x) == 0;
+}
+*/
+
 namespace PortableRuntime
 {
 
 // Change byte order from big to little or little to big.
 static uint16_t uint16_swap(uint16_t value) NOEXCEPT
 {
+#ifdef _MSC_VER
+    return _byteswap_ushort(value);
+#else
     return ((value >> 8) & 0x00ff) | ((value << 8) & 0xff00);
+#endif
 }
 
 static uint32_t uint32_swap(uint32_t value) NOEXCEPT
 {
+#ifdef _MSC_VER
+    return _byteswap_ulong(value);
+#else
     return ((value >> 24) & 0x000000ff) | ((value >> 8) & 0x0000ff00) | ((value << 8) & 0x00ff0000) | ((value << 24) & 0xff000000);
+#endif
 }
 
 // Change byte order from big/little to native format.
