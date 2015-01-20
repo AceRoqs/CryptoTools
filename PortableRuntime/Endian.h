e516f422b092dc7c2310af7b6b125c6f5bb9d44b
Toby Jones <ace_roqs@yahoo.com>
2015-01-19 18:45:27 -0800

    Add Endian code from CrappyCrypto.

---
+++ PortableRuntime/Endian.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace PortableRuntime
+{
+
+uint16_t lswap16(uint16_t value) NOEXCEPT;
+uint16_t bswap16(uint16_t value) NOEXCEPT;
+uint32_t lswap32(uint32_t value) NOEXCEPT;
+uint32_t bswap32(uint32_t value) NOEXCEPT;
+
+}
+
