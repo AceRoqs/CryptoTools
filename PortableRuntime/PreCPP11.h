58734a13d39fe2b359187067a3d417b754b69704
Toby Jones <aceroqs@users.noreply.github.com>
2015-12-22 16:34:37 -0800

    Finalize deletion of PreCPP11.h.

--- PortableRuntime/PreCPP11.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Define C++11 constructs not available in pre-C++11 compilers.
-
-#define OVERRIDE override
-#define EQUALS_DELETE = delete
-#define NOEXCEPT noexcept
-#define UTF8(x) u8##x
-
