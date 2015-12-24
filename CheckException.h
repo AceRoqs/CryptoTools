2830c1e2a5bd725812513cafdc857406bcc53ed9
Toby Jones <aceroqs@users.noreply.github.com>
2015-12-24 11:59:57 -0800

    Squashed 'PortableRuntime/' changes from e5330a6..6195950
    6195950 Merge commit '426b2642655475f376df67a38ceee08f5b7d77e8'
    77c36ad Finalize deletion of PreCPP11.h.
    b488b22 Remove PreCPP11.h now that compiler support exists.
    062ab19 Convert project to Visual Studio Community 2015.
    6808b36 Exception specification not allowed on typedef.
    b500b2d Work around MSVC issue when trying to debug exceptions in DEBUG builds.
    99090cc Allow a message with an exception.  Considering making this the default...maybe."
    
    git-subtree-dir: PortableRuntime
    git-subtree-split: 6195950dc824682aedf6440fb0f80dd6d1cae5ad

--- CheckException.h
+++ CheckException.h
@@ -4,12 +4,39 @@ namespace PortableRuntime
 {
 
 // TODO: Move implementation to .cpp file.
+// TODO: Consider replacing all calls to this function with calls to the function that accepts a message.
 inline void check_exception(bool result)
 {
     if(!result)
     {
+#if defined(_MSC_VER) && !defined(NDEBUG)
+        // Work around MSVC issue where the abort() message box is displayed instead of the assert messagebox.
+        // This makes the abort/retry/ignore of the dialog not work for the "ignore" case.  Since the abort dialog is called
+        // even for console apps where blocking for a GUI response is not ideal, just mimic the behavior as it is no worse.
+        // This also has the nice side effect of printing error information to the debugger window.
+        _set_error_mode(_OUT_TO_MSGBOX);
+#endif
+        // Force break.
         assert(false);
         throw std::exception();
+    }
+}
+
+// TODO: Move implementation to .cpp file.
+inline void check_exception(bool result, const char* message)
+{
+    if(!result)
+    {
+#if defined(_MSC_VER) && !defined(NDEBUG)
+        // Work around MSVC issue where the abort() message box is displayed instead of the assert messagebox.
+        // This makes the abort/retry/ignore of the dialog not work for the "ignore" case.  Since the abort dialog is called
+        // even for console apps where blocking for a GUI response is not ideal, just mimic the behavior as it is no worse.
+        // This also has the nice side effect of printing error information to the debugger window.
+        _set_error_mode(_OUT_TO_MSGBOX);
+#endif
+        // Force break.
+        assert(false);
+        throw std::exception(message);
     }
 }
 
