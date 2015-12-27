87f0441675c35858c902ce2a06f0439dae91a9c2
Toby Jones <aceroqs@users.noreply.github.com>
2015-12-27 12:24:55 -0800

    Fix bug where exception calls abort.  Derp.

--- CrappyCrypto/Keys.cpp
+++ CrappyCrypto/Keys.cpp
@@ -5,7 +5,7 @@
 namespace CrappyCrypto
 {
 
-void key_vector_from_key_file(_Out_writes_all_(key_vector_length) uint8_t* key_vector, size_t key_vector_length, std::basic_ifstream<uint8_t>& key_file) noexcept
+void key_vector_from_key_file(_Out_writes_all_(key_vector_length) uint8_t* key_vector, size_t key_vector_length, std::basic_ifstream<uint8_t>& key_file)
 {
     // Assumes exceptions are not enabled for stream.
     key_file.seekg(0, std::ios::end);
