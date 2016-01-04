f93187d74947cb5623cf82086ba0a9835bbe1e3a
Toby Jones <aceroqs@users.noreply.github.com>
2016-01-03 21:26:36 -0800

    Enable UTF-8 output on Windows.

--- SkipjackDecrypt/SkipjackDecrypt.cpp
+++ SkipjackDecrypt/SkipjackDecrypt.cpp
@@ -2,19 +2,24 @@
 #include <CrappyCrypto/Decrypt.h>
 #include <CrappyCrypto/Keys.h>
 #include <PortableRuntime/CheckException.h>
+#include <PortableRuntime/Unicode.h>
 
 namespace CrappyCrypto
 {
 
-void decrypt_file(_In_z_ const char* input_file_name, _In_z_ const char* output_file_name, _In_z_ const char* key_file_name);
+void decrypt_file(_In_z_ const wchar_t* input_file_name, _In_z_ const wchar_t* output_file_name, _In_z_ const wchar_t* key_file_name);
 
 }
 
-int main(int argc, _In_reads_(argc) char** argv)
+int wmain(int argc, _In_reads_(argc) wchar_t** argv)
 {
+    // Set wprintf output to UTF-8 in Windows console.
+    _setmode(_fileno(stdout), _O_U8TEXT);
+    _setmode(_fileno(stderr), _O_U8TEXT);
+
     if(argc != 4)
     {
-        std::fprintf(stderr, "Usage: %s infile outfile keyfile", argv[0]);
+        std::fwprintf(stderr, L"Usage: %s infile outfile keyfile", argv[0]);
         return 0;
     }
 
@@ -24,8 +29,7 @@ int main(int argc, _In_reads_(argc) char** argv)
     }
     catch(const std::exception& ex)
     {
-        // TODO: Under MSVC, fprintf expects ANSI, not UTF-8.
-        std::fprintf(stderr, "%s\n", ex.what());
+        std::fwprintf(stderr, L"%s\n", PortableRuntime::utf16_from_utf8(ex.what()).c_str());
         return 1;
     }
 
@@ -35,21 +39,23 @@ int main(int argc, _In_reads_(argc) char** argv)
 namespace CrappyCrypto
 {
 
-void decrypt_file(_In_z_ const char* input_file_name, _In_z_ const char* output_file_name, _In_z_ const char* key_file_name)
+void decrypt_file(_In_z_ const wchar_t* input_file_name, _In_z_ const wchar_t* output_file_name, _In_z_ const wchar_t* key_file_name)
 {
+    using namespace PortableRuntime;
+
     // Open input file.
     std::basic_ifstream<uint8_t> input_file(input_file_name, std::ios::binary);
-    PortableRuntime::check_exception(input_file.good(), (std::string("Error opening: ") + input_file_name).c_str());
+    check_exception(input_file.good(), (u8"Error opening: " + utf8_from_utf16(input_file_name)).c_str());
 
     // Open output file.
     std::basic_ofstream<uint8_t> output_file(output_file_name, std::ios::binary);
-    PortableRuntime::check_exception(output_file.good(), (std::string("Error opening: ") + output_file_name).c_str());
+    check_exception(output_file.good(), (u8"Error opening: " + utf8_from_utf16(output_file_name)).c_str());
 
-     // Open key file.
+    // Open key file.
     std::basic_ifstream<uint8_t> key_file(key_file_name, std::ios::binary);
-    PortableRuntime::check_exception(key_file.good(), (std::string("Error opening: ") + key_file_name).c_str());
+    check_exception(key_file.good(), (u8"Error opening: " + utf8_from_utf16(key_file_name)).c_str());
 
-   // Build key.
+    // Build key.
     uint8_t key_vector[CrappyCrypto::Skipjack::key_size];
     CrappyCrypto::key_vector_from_key_file(key_vector, sizeof(key_vector), key_file);
 
