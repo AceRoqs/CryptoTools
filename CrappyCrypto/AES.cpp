df263ba3ccf6a3ee41766662881eb8e89acae2ca
Toby Jones <ace_roqs@yahoo.com>
2016-04-20 21:33:35 -0700

    Get rid of non-UTF-8 characters in advance of a compiler switch.

--- CrappyCrypto/AES.cpp
+++ CrappyCrypto/AES.cpp
@@ -147,22 +147,22 @@ std::vector<uint8_t> AES_cipher(const std::vector<uint8_t>& input, const std::ve
 //Cipher(byte in[4*Nb], byte out[4*Nb], word w[Nb*(Nr+1)])
 // begin
 // byte state[4,Nb]
- 
+
 //state = in
- 
+
 //AddRoundKey(state, w[0, Nb-1]) // See Sec. 5.1.4
- 
-//for round = 1 step 1 to Nr–1
+
+//for round = 1 step 1 to Nr-1
 // SubBytes(state) // See Sec. 5.1.1
 // ShiftRows(state) // See Sec. 5.1.2
 // MixColumns(state) // See Sec. 5.1.3
 // AddRoundKey(state, w[round*Nb, (round+1)*Nb-1])
 // end for
- 
+
 //SubBytes(state)
 // ShiftRows(state)
 // AddRoundKey(state, w[Nr*Nb, (Nr+1)*Nb-1])
- 
+
 //out = state
 // end
 #endif
